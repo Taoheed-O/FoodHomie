bd742878db95234279e0fb3726cc4769acb3218d
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-07-12 12:52:43 +0100

    building a stremlit app for the generator

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title('Restaurant name generator')
 
+st.sidebar.selectbox('pick a restaurant', ('arabian', 'mexican', 'indian', 'spanish',
+                                        'italian','pakistani', 'iranian', 'american', 'british', 'african'))
